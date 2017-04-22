8a7f97be0b3790cb577d7e0ac7aa7e7e3573aca3
nithal <nithalajay@gmail.com>
2017-04-22 12:47:25 +0530

    Initial commit

---
+++ src/js/client.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+	render() {
+		return(
+			<h1>It is working</h1>
+		);
+	}
+}
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
